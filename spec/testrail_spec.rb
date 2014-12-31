1a3e1ca3bd08f393cac18355de5b61546da300f9
Joe Pinsonault <joe.pinsonanult@puppetlabs.com>
2014-12-31 13:33:00 -0800

    Added mostly empty spec tests

---
+++ spec/testrail_spec.rb
@@ -0,0 +1,33 @@
+require_relative "../lib/beaker_testrail"
+require_relative "../lib/testrail"
+require_relative './spec_helper'
+
+
+describe BeakerTestrail do
+  class BTModule
+  end
+
+  before :each do
+    @testrail_credentials = {
+      "testrail_username" => "test",
+      "testrail_password" => "testpass"
+    }
+
+    stub_request(:post, "http://www.example.com/").
+      with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+      to_return(:status => 200, :body => "", :headers => {})
+  end
+
+  describe "#get_testrail_api" do
+    it "returns a APIClient object" do
+      api = BeakerTestrail::get_testrail_api(@testrail_credentials)
+      expect(api).to be_an_instance_of TestRail::APIClient
+    end
+
+    it "has the correct credentials" do
+      api = BeakerTestrail::get_testrail_api(@testrail_credentials)
+      expect(api.password).to be @testrail_credentials["testrail_password"]
+      expect(api.user).to be @testrail_credentials["testrail_username"]
+    end
+  end
+end
